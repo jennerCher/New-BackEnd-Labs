81626b03419d8a2217de66e39c7d98134fe1430e
jennerCher <162926648+jennerCher@users.noreply.github.com>
2024-12-08 18:48:27 -0500

    Update Week01VariablesAndOperationsLab.java

--- week01/Week01VariablesAndOperationsLab.java
+++ week01/Week01VariablesAndOperationsLab.java
@@ -13,45 +13,45 @@ public class Week01VariablesAndOperationsLab {
 		// 1. Create a variable to hold the quantity of 
 		//		available plane seats left on a flight		
 
-		
+		int seatsOnPlane = 35;
 		// 2. Create a variable to hold the cost of groceries at checkout
 
-		
+		double costOfGroceries = 225.34;
 		// 3. Create a variable to hold a person's middle initial
 
-	
+		char middleInitial = 'A';
 		// 4. Create a variable to hold true if it's hot outside 
 		//		and false if it's cold outside
-
+		boolean isColdOutside = true;
 		
 		// 5. Create a variable to hold a customer's first name
 
-		
+		string firstName = "Cheryl";
 		// 6. Create a variable to hold a street address
 		
-
+		string streetAddress = "2 Heritage Circle";
 		// 7. Print all variables to the console
-	
-
+		
+		
 		// 8. A customer booked 2 plane seats, 
 		//		remove 2 seats from the available seats variable
-	
+		seatsOnPlane = seatsOnPlane - 2;
 
 		// 9. Impulse candy bar purchase, add 2.15 to the grocery total
-		// 		costOfGroceries = costOfGroceries + 2.15;
-
+		costOfGroceries = costOfGroceries + 2.15;
+		
 
 		// 10.  The birth certificate was printed incorrectly, 
 		//		change the middle initial to something else
-		
+		middleInitial = "M";
 
 		// 11.  The season has changed, update the hot outside 
 		//			variable to be opposite of what it was
-
+		isColdOutside = false;
 
 		// 12.  Create a new variable called full name using the customer's first name, 
 		//			the middle initial, and a last name of your choice
-	
+		string fullName = firstName + " " + middleInitial + ". " + "Jennerjohn";
 
 		// 13.  Print a line to the console that introduces the customer and says they live
 		// 			at the address variable
